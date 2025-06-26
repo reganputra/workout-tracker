4d3b9923ff3d979e6914290793b893fbbfe2a528
reganputra <ramadhanregan334@gmail.com>
2025-06-27 05:53:07 +0700

    Add method to retrieve user by token and implement anonymous user check

--- store/user_store.go
+++ store/user_store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"crypto/sha256"
 	"database/sql"
 	"errors"
 	"golang.org/x/crypto/bcrypt"
@@ -45,6 +46,12 @@ type User struct {
 	UpdatedAt    time.Time `json:"updated_at"`
 }
 
+var AnonymousUser = &User{}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
+}
+
 type PostgresUserStore struct {
 	db *sql.DB
 }
@@ -57,6 +64,7 @@ type UserStore interface {
 	CreateUser(*User) error
 	GetUserByName(username string) (*User, error)
 	UpdateUser(*User) error
+	GetUserToken(scope, tokenPlaintTextPassword string) (*User, error)
 }
 
 func (store *PostgresUserStore) CreateUser(user *User) error {
@@ -109,4 +117,30 @@ func (store *PostgresUserStore) UpdateUser(user *User) error {
 		return sql.ErrNoRows
 	}
 	return nil
+}
+
+func (store *PostgresUserStore) GetUserToken(scope, tokenPlaintTextPassword string) (*User, error) {
+	tokenHash := sha256.Sum256([]byte(tokenPlaintTextPassword))
+	query := "SELECT u.id, u.username, u.email, u.password_hash, u.bio, u.created_at, u.updated_at " +
+		"FROM users u INNER JOIN tokens t ON t.user_id = u.id WHERE t.hash = $1 AND t.scopes = $2 AND t.expired > $3"
+
+	user := &User{
+		PasswordHash: password{},
+	}
+	err := store.db.QueryRow(query, tokenHash[:], scope, time.Now()).Scan(
+		&user.Id,
+		&user.UserName,
+		&user.Email,
+		&user.PasswordHash.hash,
+		&user.Bio,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
